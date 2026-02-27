a8fad564f485f9ea85b92f343974c6b3aa4f64b2
REVATHI-SENTHILKUMAR <revathigeetha20@gmail.com>
2026-02-27 08:08:49 +0530

    hosting

--- DAY 2/task1.py
+++ DAY 2/task1.py
@@ -1,12 +1,9 @@
-# Define three numbers
 a = 15
 b = 42
 c = 27
 
-# Method 1: Using the built-in max function (Recommended)
 biggest = max(a, b, c)
 
-# Method 2: Using conditional logic
 if (a >= b) and (a >= c):
     largest = a
 elif (b >= a) and (b >= c):
