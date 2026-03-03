f1634d8611e9846fd44dbb3f14cc5822e5ce9a9d
REVATHI-SENTHILKUMAR <revathigeetha20@gmail.com>
2026-03-03 10:47:59 +0530

    hosting

---
+++ DAY 6/practice.py
@@ -0,0 +1,31 @@
+# Creating a tuple
+fruits = ("apple", "banana", "cherry")
+
+# Accessing elements (just like a list)
+print(fruits[0])  # Output: apple
+
+
+
+#The "Immutable" Rule
+coordinates = (10, 20)
+
+# This will cause an error:
+# coordinates[0] = 15
+
+
+
+to assign multiple variables at once. 
+# Packing a tuple
+person = ("Alice", 25, "Engineer")
+
+
+# Unpacking the tuple into variables
+name, age, profession = person
+
+
+print(name)        # Output: revathi
+print(profession)  # Output: Engineer
+
+
+not_a_tuple = ("apple")  # This is a String
+is_a_tuple = ("apple",)  # This is a Tuple
