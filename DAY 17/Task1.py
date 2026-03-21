bfae9b4f501823dc0d74f63dc4fb105b0dfb6823
REVATHI-SENTHILKUMAR <revathigeetha20@gmail.com>
2026-03-21 12:34:54 +0530

    hosting

---
+++ DAY 17/Task1.py
@@ -0,0 +1,40 @@
+# Function with Parameter and Return
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(5, 10)
+print(f"Total: {result}")
+
+# While Loop
+
+count = 3
+while count > 0:
+    print(f"Counting: {count}")
+    count -= 1
+    
+#For Loop
+
+for i in range(1, 4):
+    print(f"Iteration {i}")
+    
+#Collections (List, Set, Tuple, Dictionary)
+
+ # List 
+fruits = ["apple", "banana", "cherry"]
+
+# Set 
+unique_nums = {1, 2, 2, 3} 
+
+# Tuple 
+coords = (10, 20)
+
+# Dictionary 
+user = {"name": "revathi", "age": 25}
+
+# lambda Function
+
+square = lambda x: x * x
+print(f"Square of 4: {square(4)}")
+
+   
